80b01189e7c8f972b1c0bcce1f35b5f4b76eae66
Theelipan Prabakar <theelipan@gmail.com>
2020-08-28 19:45:18 -0400

    create server and connect index page

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 const path = require("path");
-const fs = require("fs");
-const server = require("http");
 
-server.createServer;
+const fs = require("fs");
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    console.log("working");
+    fs.readFile(path.join(__dirname, "public", "index.html"), (err, content) => {
+      if (err) throw err;
+      res.writeHead(200, { "content-type": "text/html" });
+      res.end(content);
+    });
+  }
+});
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, console.log(`Server running on port: ${PORT}`));
