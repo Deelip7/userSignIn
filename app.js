ce1f6f1242baad3218a69126b64b1dd7c6b153cf
Theelipan Prabakar <theelipan@gmail.com>
2020-09-05 15:04:07 -0400

    connect to MongoDB

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
+const mongoose = require("mongoose");
 
 // ---- Serving static CSS and Image files
 app.use(express.static(__dirname + "/public"));
+
+// ---- MongoDB Config
+const db = require("./config/keys").MongoURI;
+// ---- Connect to DB
+mongoose
+  .connect(db, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true })
+  .then(() => console.log("Connected to DB"))
+  .catch((err) => console.log(err));
 
 // ---- EJS
 app.use(expressLayouts);
 app.set("view engine", "ejs");
 
 //---- Connect routers
+
 app.use("/", require("./routes/index"));
 app.use("/users", require("./routes/users"));
 
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  MongoURI: "mongodb+srv://user7:user7@cluster0.gthxj.mongodb.net/<dbname>?retryWrites=true&w=majority",
+};
