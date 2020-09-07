e6eea828d561b046435c30e84ecb0cf4f0c01eda
Theelipan Prabakar <theelipan@gmail.com>
2020-09-07 19:06:56 -0400

    validate register password

--- routes/users.js
+++ routes/users.js
@@ -7,6 +7,25 @@ router.get("/register", (req, res) => res.render("register"));
 
 router.post("/register", (req, res) => {
   const { name, email, password, password2 } = req.body;
+
+  const passErrorMsg = passwordValidation(password, password2);
+
+  if (passErrorMsg.length > 0) {
+    res.render("register", { passErrorMsg });
+  } else {
+    res.send("Pass");
+  }
 });
+//Form Validation
+
+function passwordValidation(pass1, pass2) {
+  let errors = [];
+  if (pass1 !== pass2) {
+    errors.push({ msg: "Passwords do not match" });
+  } else if (pass1.length < 7) {
+    errors.push({ msg: "Password should be at least 7 characters" });
+  }
+  return errors;
+}
 
 module.exports = router;
